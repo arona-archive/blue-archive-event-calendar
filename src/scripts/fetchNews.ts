f754133d503c07a627a4c39e89cfaed6b081abed
sapphire <info@sapphire.sh>
2022-07-15 00:03:47 +0900

    use gaxios

--- src/scripts/fetchNews.ts
+++ src/scripts/fetchNews.ts
@@ -1,7 +1,8 @@
-import axios from 'axios';
+import { request } from 'gaxios';
+import { News } from '../types';
 
 export const fetchNews = async () => {
-	const res = await axios.request({
+	const res = await request<{ data: { items: News[] } }>({
 		url: process.env.NOTICE_URL,
 	});
 	return res.data;
