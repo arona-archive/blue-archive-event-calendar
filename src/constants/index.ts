9880b7a3a7c08ab80abca67bcc0c4861a7eab237
sapphire <info@sapphire.sh>
2025-01-08 13:32:51 +0900

    update filter

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -43,7 +43,7 @@ export const NOTICE_TITLE_FILTER: { value: string; position: 'start' | 'end' }[]
 ];
 export const NOTICE_ID_FILTER: number[] = [
 	211, 231, 243, 280, 290, 286, 296, 297, 295, 313, 312, 333, 336, 353, 358, 359, 360, 373, 381, 386, 395, 396, 400,
-	402, 404, 405, 420, 424, 426, 427, 428, 429, 434, 435, 444,
+	402, 404, 405, 420, 424, 426, 427, 428, 429, 434, 435, 444, 516,
 ];
 
 export enum NoticeType {
